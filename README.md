6b26462edd6ff39faadf37c34d35bc412d9ca961
alirani <elamrani.ali@live.fr>
2024-07-18 08:11:01 +0200

    feat : Header
    squashed commits :
      center mob navigation items
      add mob nav
      layout + reset & globals css
      header
      bloom logo
      readme
      install sass & tabler icons

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ component list :
 - [ ] Hero  
 - [ ] Result Card 
 - [ ] Pagination 
+- [x] Icons
 
 The idea is to first build a mobile version and make it stretch into a desktop version.
 
@@ -18,3 +19,9 @@ Some points :
 
 - the mobile version in Figma seems a bit wrong : Footer is replaced by external offers
 - the fonts aren't consistent across the page : `Open Sans` for Header, `Manrope` for Search Form & Pagination & Footer, `DM Sans` for Search Results
+
+# During
+
+Some points :
+- the Header is kinda tricky, for this test it's better to just put a margin and adjust other components around, as is, but for a prod project, I would ask the designer if he plans on adding pages where that margin isn't there, in that case, it would be a good idea to add a margin condition to the component.
+- Same for background in the figma it's defined in a block in absolute position, in reality, it's better to just attach that to the body and adjust the percentage accordingly to match desktop and mobile view. Here 50% seems ok.
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import Link from 'next/link'
+import BloomIcon from '../Icon/Bloom'
+import styles from './Header.module.scss'
+import { Burger, Button, Drawer, NavLink } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
+import { IconBellFilled } from '@tabler/icons-react'
+
+interface NavLink {
+  link: string
+  label: string
+  button?: boolean
+  icon?: boolean
+}
+
+interface HeaderProps {
+  navItems: NavLink[]
+}
+
+const getNavItems = (navItems: NavLink[], centered?: boolean) => {
+  return (
+    <ul className={centered ? styles.centeredList : ''}>
+      {navItems.map((navItem) => (
+        <li key={navItem.label}>
+          {navItem.button ? (
+            <Button
+              variant='filled'
+              color='#12564B'
+              radius='xl'
+              component={Link}
+              href={navItem.link}
+            >
+              {navItem.label}
+            </Button>
+          ) : (
+            <Button
+              variant='transparent'
+              color='#101010'
+              px={8}
+              radius='xl'
+              leftSection={navItem.icon ? <IconBellFilled color='#CDEBE7' /> : null}
+              component={Link}
+              href={navItem.link}
+            >
+              {navItem.label}
+            </Button>
+          )}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+const Header = (props: HeaderProps) => {
+  const { navItems } = props
+
+  const [opened, { open, close }] = useDisclosure()
+  const centered = true
+  
+  return (
+    <header className={styles.header}>
+      <BloomIcon />
+      <Burger
+        className={styles.icon}
+        opened={opened}
+        onClick={open}
+        aria-label='Open navigation'
+      />
+      <nav className={styles.navBar}>
+        {getNavItems(navItems)}
+        <Drawer
+          className={styles.mobNav}
+          opened={opened}
+          onClose={close}
+          title={<BloomIcon />}
+        >
+          {getNavItems(navItems, centered)}
+        </Drawer>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
